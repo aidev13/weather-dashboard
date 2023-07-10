6d8c53f49ca22ea55ba57aa8deb76bb643a0c721
David Liebherr <ambition.aidev@gmail.com>
2023-07-10 17:10:08 -0500

    beginning code. added weather apikey and fecth functions. add small css styles to help give definition. added html start up structor

---
+++ script.js
@@ -0,0 +1,37 @@
+var citySearched = document.getElementById('#citySearched')
+var searchBtn = document.querySelector('button')
+var apiKey = '59a3c0db12e1f890c3e94259c9168e7f'
+
+function getWeatherByFetch(cityName) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (weatherData) {
+            var h2 = document.createElement('h2')
+            h2.innerText = cityName
+            var ul = document.createElement('ul')
+            var outdoorTemp = document.createElement('li')
+            var skyConditions = document.createElement('li')
+            var wind = document.createElement('li')
+
+            outdoorTemp.innerText = "Temp: " + weatherData.main.temp
+            skyConditions.innerText = "Sky Overview: " + weatherData.weather[0].main
+            wind.innerText = "Wind Conditions: " + weatherData.wind.speed + " mph"
+
+            ul.appendChild(outdoorTemp)
+            ul.appendChild(skyConditions)
+            ul.appendChild(wind)
+            document.body.appendChild(h2)
+            document.body.appendChild(ul)
+
+
+        })
+}
+
+getWeatherByFetch('Oconomowoc')
+searchBtn.addEventListener('click', function(event) {
+
+})
+
+
